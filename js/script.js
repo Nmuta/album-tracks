b743d26b1c0df7dbd5556ad0324f9a08147931ce
Brandon Jewell <brandon.royce.jewell@gmail.com>
2015-12-10 10:34:37 -0700

    finished

--- js/script.js
+++ js/script.js
@@ -6,36 +6,47 @@ $(document).ready(function(){
      dataType: "JSON"
   })
 
-
-
-  var x = ( Math.floor(Math.random() * 4) + 0)
-  var y = ( Math.floor(Math.random() * 3) + 0)
-  var z = ( Math.floor(Math.random() * 5) + 0)
-
+//OBJECT ITERATION vvvv
   randomalbum.done(function(response){
-     var getalbums = response["results"]
-     var arr = []
-     var artists = [];
-     for (var i = 0; i < getalbums.length; i++) {
+         var getalbums = response["results"]
+         var arr = []
+         for (var i = 0; i < getalbums.length; i++) {
          arr.push(getalbums[i]["cover_art"])
-         var id = getalbums[i]["id"]
          var art = getalbums[i]["artist"];
-         artists.push(art);
-
          var title = getalbums[i]["title"]
-         $("#albumsplaylist").append("<img id='"+id+"'class='playlistpics'src='images/"+arr[i]+"'/>")
-         $("#"+id).on("click", function () {
-            console.log(artists);
+         $("#albumsplaylist").append("<img id='"+i+"' value='"+art+"'class='playlistpics'src='images/"+arr[i]+"'/>")
+         $("#"+i).click( function () {
+            $("#box").append(getalbums[event.target.id]["artist"]+": "+getalbums[event.target.id]["title"]+"<br>")
          });
+       }
 
-     }
+
+//BUTTONSvvvv
+
+      $("#clear").click(function(){
+        $("#box").html("")
+      })
+
+      $("#sub").click(function(){
+        var post = $.ajax({
+          url:"https://lit-fortress-6467.herokuapp.com/post",
+          method: "POST"
+        })
+        post.done(function(response){
+          $("#box").html("POST request successful! woop woop. shimmy shimmy. bang bang.")
+        })
+      })
+
+
+//SHUFFLEvvvv
+
+       $(document).ready(function(){
+        var x = ( Math.floor(Math.random() * 3) + 0)
+        var y = ( Math.floor(Math.random() * 4) + 0)
+        var z = ( Math.floor(Math.random() * 5) + 0)
         $("#uno").append("<img id='alb'src='images/"+arr[x]+"'/>")
         $("#uno").append("<img  id='alb'src='images/"+arr[y]+"'/>")
         $("#uno").append("<img  id='alb'src='images/"+arr[z]+"'/>")
-
-
-
-
-
+      })
   })
 })
