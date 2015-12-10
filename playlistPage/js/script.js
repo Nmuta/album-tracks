2c30393c690d90a70d9c86507befc1c8baa62a31
Josh Silverstein <silverjosh90@gmail.com>
2015-12-10 12:50:51 -0700

    Finished All Modules

--- playlistPage/js/script.js
+++ playlistPage/js/script.js
@@ -1,17 +1,26 @@
-var call = $.ajax({
+$(document).ready(function(){
+  var call = $.ajax({
     url: "https://lit-fortress-6467.herokuapp.com/object",
     method: "GET",
     dataType: "Json"
 })
 
 call.done(function(response){
-  console.log(response)
   var j = response["results"]
 
 for(var i = 0; i < j.length; i++) {
-  $("#pictures").append("<img class='pic' src='../images/"+j[i]["cover_art"]+"'>")
-   console.log(j[i]["title"])
+  $("#pictures").append("<img class='pic' id='"+j[i]["artist"]+"'"+" src='../images/"+j[i]["cover_art"]+"'>")
+
 }
+
+$(document).on("click", ".pic", function(){
+var h = response["results"]
+for (var i = 0; i < h.length; i++) {
+  if(h[i]["artist"] == this.id){
+    $('#box').append("<p class='pPad'>"+ h[i]["artist"]+": "+h[i]["title"]+"</p>")
+  }
+}
+});
 });
 
 var call = $.ajax({
@@ -20,5 +29,11 @@ var call = $.ajax({
 })
 
 call.done(function(response){
-  console.log("You did it!")
+  console.log(response)
 })
+
+$("#clear").click(function(){
+  $(".pPad").html(" ")
+});
+
+});
